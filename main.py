320bd1ab30b2a386c862fd39dc2ef352771b0b8e
Justin Lee <justinjustin0425@gmail.com>
2026-01-19 12:12:02 -0500

    added minibatching

--- main.py
+++ main.py
@@ -4,7 +4,6 @@ from utils import cross_entropy_loss
 from sklearn.datasets import fetch_openml
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import OneHotEncoder
-import pandas as pd
 mnist = fetch_openml('mnist_784', version=1, as_frame=False)
 mnist.data = mnist.data / 255.0
 train_data, test_data, train_target, test_target = train_test_split(
@@ -17,17 +16,19 @@ print(train_target.shape)
 print(mnist.data[2])
 print(mnist.target[2])
 model = mlp()
-epochs = 20
+epochs = 10
+batch_size = 64
 for epoch in range(epochs):
-    for i in range(train_data.shape[0]):
-        x = train_data[i].reshape(1, -1)
-        y = train_target_oh[i].reshape(1, -1)
+    for i in range(0, train_data.shape[0], batch_size): 
+        x = train_data[i:i+batch_size]
+        y = train_target_oh[i:i+batch_size]
+        #print(y)
         predictions = model.forward(x)
-        loss = cross_entropy_loss(predictions, np.array([np.argmax(y)]))
-        dout = predictions - y.reshape(1, -1)
+        loss = cross_entropy_loss(predictions, y)
+        dout = predictions - y
         model.backward(x, dout)
-        if (i+1) % 1000 == 0:
-            print(f"Epoch {epoch+1}, Sample {i+1}, Loss: {loss:.8f}")
+        if (i) % 1600 == 0:
+            print(f"Epoch {epoch}, Sample {i+1}, Loss: {loss:.8f}")
 correct = 0
 for i in range(test_data.shape[0]):
     x = test_data[i].reshape(1, -1)
--- utils.py
+++ utils.py
@@ -6,11 +6,12 @@ def relu_derivative(input):
 def softmax(input):
     exp_input = np.exp(input - np.max(input, axis=1, keepdims=True))
     return exp_input / np.sum(exp_input, axis=1, keepdims=True)
-def cross_entropy_loss(predictions, target, epsilon=1e-12):
-    m = target.shape[0]
-    predictions = np.clip(predictions, epsilon, 1. - epsilon)
-    log_likelihood = -np.log(predictions[range(m), target])
-    loss = np.sum(log_likelihood) / m
+def cross_entropy_loss(predictions, targets):
+    sample_count = predictions.shape[0]
+    #print(targets)
+    clipped_preds = np.clip(predictions, 1e-15, 1 - 1e-15)
+    #print(clipped_preds)
+    log_likelihood = sum(-np.log(clipped_preds) * targets)
+    loss = np.sum(log_likelihood) / sample_count
     return loss
-
 
